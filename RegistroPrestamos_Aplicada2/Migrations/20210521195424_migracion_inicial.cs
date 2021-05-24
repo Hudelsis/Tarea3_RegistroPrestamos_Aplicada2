217272f4a6f759400a87dbf5d161ab4579bc7d85
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2021-05-24 17:46:15 +0200

    Agregar archivos de proyecto.

---
+++ RegistroPrestamos_Aplicada2/Migrations/20210521195424_migracion_inicial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroPrestamos_Aplicada2.Migrations
+{
+    public partial class migracion_inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    PersonaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: false),
+                    Cedula = table.Column<string>(type: "TEXT", nullable: false),
+                    Direccion = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Balance = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.PersonaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    PrestamoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PersonaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: false),
+                    Monto = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Balance = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.PrestamoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+        }
+    }
+}
---
+++ RegistroPrestamos_Aplicada2/Validations/NombresValidacion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamos_Aplicada2.Validations
+{
+    public class NombresValidacion : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string cadena = value as string;
+
+            if (!string.IsNullOrWhiteSpace(cadena))
+            {
+                //if (cadena.Length <= 0)
+                //    return new ValidationResult("Debes poner un Nombre");
+
+
+                foreach (var caracter in cadena)
+                {
+                    if (!char.IsLetter(caracter) && !char.IsWhiteSpace(caracter))
+                        return new ValidationResult("El nombre solo puede tener letras");
+                }
+
+                return ValidationResult.Success;
+
+            }
+            return new ValidationResult("Debes poner un Nombre");
+        }
+    }
+}
